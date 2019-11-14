e4ab5c405651c23ec74a70586a3385e2d49d8c4e
Frank van Woerden <frank@insyde.nl>
2019-11-14 11:58:36 +0100

    Add names to the calculator buttons

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Calculator
+﻿using System.Windows;
+
+namespace Calculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +10,18 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SevenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if ((string) ResultLabel.Content == "0")
+            {
+                ResultLabel.Content = "7";
+            }
+            else
+            {
+                ResultLabel.Content += "7";
+            }
         }
     }
 }
