1e51b00c85a1d59be85c55d01f2b95503d2e6f5f
Frank van Woerden <frank@insyde.nl>
2019-12-05 11:14:31 +0100

    Have the game start with a random word from a hardcoded list

--- Hangman/Game/WordProvider.cs
+++ Hangman/Game/WordProvider.cs
@@ -1,10 +1,16 @@
+using System;
+
 namespace Hangman
 {
     public class WordProvider
     {
+        private static string[] _words = {"hangman", "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "kiosk", "klutz", "memento", "mystify", "numbskull", "ostracize", "oxygen", "pajama", "phlegm", "pixel", "polka", "quad", "quip", "rhythmic", "rogue", "sphinx", "squawk", "swivel", "toady", "twelfth", "unzip", "waxy", "wildebeest", "yacht", "zealous", "zigzag", "zippy", "zombie", "gandalf", "apartment"};
+        private static Random _rand = new Random();
+        
         public static string GetRandomWord()
         {
-            return "hangman";
+            int index = _rand.Next(_words.Length);
+            return _words[index].ToLower();
         }
     }
 }
