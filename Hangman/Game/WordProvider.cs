1faf9fce05d9e57213c4b19391587614e8dfd089
Frank van Woerden <frank@insyde.nl>
2019-12-06 09:06:32 +0100

    Add more words to the hangman game

--- Hangman/Game/WordProvider.cs
+++ Hangman/Game/WordProvider.cs
@@ -4,7 +4,7 @@ namespace Hangman
 {
     public class WordProvider
     {
-        private static string[] _words = {"hangman", "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "kiosk", "klutz", "memento", "mystify", "numbskull", "ostracize", "oxygen", "pajama", "phlegm", "pixel", "polka", "quad", "quip", "rhythmic", "rogue", "sphinx", "squawk", "swivel", "toady", "twelfth", "unzip", "waxy", "wildebeest", "yacht", "zealous", "zigzag", "zippy", "zombie", "gandalf", "apartment"};
+        private static string[] _words = {"hangman", "awkward", "bagpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "fjord", "gazebo", "gypsy", "haiku", "haphazard", "hyphen", "ivory", "jazzy", "jiffy", "jinx", "jukebox", "kayak", "kiosk", "klutz", "memento", "mystify", "numbskull", "ostracize", "oxygen", "pajama", "phlegm", "pixel", "polka", "quad", "quip", "rhythmic", "rogue", "sphinx", "squawk", "swivel", "toady", "twelfth", "unzip", "waxy", "wildebeest", "yacht", "zealous", "zigzag", "zippy", "zombie", "gandalf", "apartment", "outstanding", "development", "situation", "technicality", "computer", "christmas", "festivities", "dungeon", "dragon", "dividing"};
         private static Random _rand = new Random();
         
         public static string GetRandomWord()
