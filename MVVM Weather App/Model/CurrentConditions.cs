33778d5dc2a8a7ea087809e52e85640570b980a2
Frank van Woerden <frank@insyde.nl>
2019-12-10 16:45:40 +0100

    Create the Models of the app

---
+++ MVVM Weather App/Model/CurrentConditions.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace MVVM_Weather_App.Model
+{
+    public class TemperatureUnits
+    {
+        public int Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public TemperatureUnits Metric { get; set; }
+        public TemperatureUnits Imperial { get; set; }
+    }
+
+    public class CurrentConditions
+    {
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public object PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+    }
+}
