5a31e0a4efa49dba987f3b9cfc02aeaa78a4bb05
Frank van Woerden <frank@insyde.nl>
2019-12-10 08:44:58 +0100

    Add a Model View Viewmodel tutorial project

---
+++ MVVM Weather App/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVM_Weather_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MVVM Weather App/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVM_Weather_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
