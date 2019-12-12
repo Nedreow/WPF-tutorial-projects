9d18926f978896260029794fb6dc693d3be4ffe6
Frank van Woerden <frank@insyde.nl>
2019-12-12 15:34:21 +0100

    Add view to the weather app

---
+++ MVVM Weather App/View/WeatherWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace MVVM_Weather_App.View
+{
+    /// <summary>
+    /// Interaction logic for WeatherWindow.xaml
+    /// </summary>
+    public partial class WeatherWindow : Window
+    {
+        public WeatherWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
