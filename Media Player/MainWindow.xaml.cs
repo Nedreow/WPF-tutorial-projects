278ee949477f8b72a0a79cf80c8529149872ffc7
Frank van Woerden <frank@insyde.nl>
2020-01-16 09:31:43 +0100

    Allow the player to load, play, pause and stop Media Player

--- Media Player/MainWindow.xaml.cs
+++ Media Player/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Media_Player
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace Media_Player
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +12,35 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Choose Media";
+            if (dialog.ShowDialog() == true)
+            {
+                Media.Source = new Uri(dialog.FileName);
+                MediaName.Text = dialog.FileName;
+            }
+        }
+        
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.Source != null)
+                Media.Play();
+        }
+ 
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.CanPause)
+                Media.Pause();
+        }
+ 
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Media.Source != null)
+                Media.Stop();
         }
     }
 }
