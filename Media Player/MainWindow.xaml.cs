4a1a4d259171a88781bc83dd91ae5136328a14a0
Frank van Woerden <frank@insyde.nl>
2020-01-16 09:09:21 +0100

    Set up the media player project and set up the xaml for the app Media Player

---
+++ Media Player/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Media_Player
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
